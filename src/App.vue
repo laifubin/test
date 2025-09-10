6054d3ef6b79a7ade9b2c0eb26c30d71aa62d0c3
laifubin <laifubin@yeah.net>
2025-09-10 23:46:21 +0800

    fix: 1

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ console.log('test:', sum)
 </script>
 
 <template>
-  <h1 style="color: rebeccapurple;">You did it!</h1>
+  <h1 style="color: rebeccapurple;">You did it! => 1</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
